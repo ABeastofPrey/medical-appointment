21d822b98b310cb0c71d46b9ff35c485cf7ecc65
ABeastofPrey <abeastofprey@ABeastofPreydeMacBook-Pro.local>
2019-12-01 15:44:38 +0800

    remove log

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
-// import { Loging } from './login/Login';
+import { Loging } from './login/Login';
 import { Appointment } from './appointment/Appointment';
-// import { Information } from './information/Information';
+import { Information } from './information/Information';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <div>
-          <span>here</span>
+          <Loging />
+          <Appointment />
+          <Information />
         </div>
       </header>
     </div>
