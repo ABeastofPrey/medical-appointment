d08562b5c72d1785c75a6ab7ee9c80f34bf93ed0
ABeastofPrey <abeastofprey@ABeastofPreydeMacBook-Pro.local>
2019-12-01 17:36:18 +0800

    add ant-mobine

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,9 @@ import './App.scss';
 import { Loging } from './login/Login';
 import { Appointment } from './appointment/Appointment';
 import { Information } from './information/Information';
+import { Button } from 'antd-mobile';
+
+import 'antd-mobile/dist/antd-mobile.css';
 
 const App: React.FC = () => {
   return (
@@ -12,6 +15,7 @@ const App: React.FC = () => {
           <Loging />
           <Appointment />
           <Information />
+          <Button>Ant Button</Button>
         </div>
       </header>
     </div>
