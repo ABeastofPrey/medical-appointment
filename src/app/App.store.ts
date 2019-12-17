39018d084cad67cb004346ed11ffa51c078ed470
ABeastofPrey <abeastofprey@ABeastofPreydeMacBook-Pro.local>
2019-12-17 21:14:01 +0800

    remove uppercase filename

--- src/app/App.store.ts
+++ src/app/App.store.ts
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import { rootReducer } from "./App.reducers";
-import { rootEpic, epicMiddleware } from './App.epics';
+import { rootReducer } from "./app.reducers";
+import { rootEpic, epicMiddleware } from './app.epics';
 
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
--- src/app/App.store.tsx
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import { rootReducer } from "./app.reducers";
-import { rootEpic, epicMiddleware } from './app.epics';
-
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export const appStore = createStore(rootReducer, composeEnhancers(applyMiddleware(epicMiddleware)) );
-
-epicMiddleware.run(rootEpic);
