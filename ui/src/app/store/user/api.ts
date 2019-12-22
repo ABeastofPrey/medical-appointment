b76f766d47183cb7e0438b1b485434f4bcbb6b8d
ABeastofPrey <abeastofprey@ABeastofPreydeMacBook-Pro.local>
2019-12-22 10:09:56 +0800

    remove user

--- ui/src/app/store/user/api.ts
+++
@@ -1,3 +0,0 @@
-import { getData } from '../../services/api.service';
-
-export const getVcode = _phone => getData(`vcode?phone=${_phone}`);
--- ui/src/app/store/user/selectors.ts
+++
@@ -1,5 +0,0 @@
-export const getUserState = state => state.users;
-
-export const selectVcode = state => getUserState(state).vcode;
-
-export const selectPhone = state => getUserState(state).phone;
