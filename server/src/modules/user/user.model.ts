fef24a271e73b78086ee478c64dfbfc6a671499d
Wang1 <Wenbo.Wang1@kuka.com>
2019-12-24 13:36:22 +0800

    update user schema

--- server/src/modules/user/user.model.ts
+++ server/src/modules/user/user.model.ts
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
-const { ObjectId } = mongoose.Types;
 
-const userSchema = new Schema({
+const user = {
     name: { type: String, sparse: true, trim: true },
     phone: { type: Number, required: [true, 'Phone cannot be empty'] },
     vcode: { type: Number, required: [true, 'Validation code cannot be empty'] },
     disabled: { type: Boolean, default: false },
     isLogin: { type: Boolean, default: true }
-}, {
+};
+
+const userSchema = new Schema<typeof user>(user, {
     timestamps: true
 });
 
